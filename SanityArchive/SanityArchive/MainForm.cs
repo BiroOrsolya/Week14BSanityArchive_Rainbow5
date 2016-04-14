4f964dca1bfd64a607de6cbf0d9943051a463406
Daniel Koics <koics.dani@gmail.com>
2016-04-14 12:37:19 +0200

    Readd a necessary using directive and a '}' character removed by merge-commit

--- SanityArchive/SanityArchive/MainForm.cs
+++ SanityArchive/SanityArchive/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
 namespace SanityArchive
@@ -25,26 +26,28 @@ namespace SanityArchive
             }
         }
 
-		private void encryptButton_Click (object sender, EventArgs e) {
-			string[] fileList = {"C:/GDrive/fixip.jpg", "C:/GDrive/train.wav"};
-			string ec = fileList[0].Substring(0, fileList[0].Length - 3) + "ec";
-			string sKey = "12345678";
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            string[] fileList = { "C:/GDrive/fixip.jpg", "C:/GDrive/train.wav" };
+            string ec = fileList[0].Substring(0, fileList[0].Length - 3) + "ec";
+            string sKey = "12345678";
 
-			FileStream fsInput = new FileStream(fileList[0], FileMode.Open, FileAccess.Read);
-			FileStream fsEncrypted = new FileStream(ec, FileMode.Create, FileAccess.Write);
+            FileStream fsInput = new FileStream(fileList[0], FileMode.Open, FileAccess.Read);
+            FileStream fsEncrypted = new FileStream(ec, FileMode.Create, FileAccess.Write);
 
-			DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
-			DES.Key = Encoding.ASCII.GetBytes(sKey);
-			DES.IV = Encoding.ASCII.GetBytes(sKey);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = Encoding.ASCII.GetBytes(sKey);
+            DES.IV = Encoding.ASCII.GetBytes(sKey);
 
-			ICryptoTransform transform = DES.CreateEncryptor();
-			CryptoStream crStream = new CryptoStream(fsEncrypted, transform, CryptoStreamMode.Write);
+            ICryptoTransform transform = DES.CreateEncryptor();
+            CryptoStream crStream = new CryptoStream(fsEncrypted, transform, CryptoStreamMode.Write);
 
-			byte[] bytearrayinput = new byte[fsInput.Length - 1];
-			fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
-			crStream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            byte[] bytearrayinput = new byte[fsInput.Length - 1];
+            fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+            crStream.Write(bytearrayinput, 0, bytearrayinput.Length);
 
 
-            
+
         }
     }
+}
