56f1cba1f8b9d712e994d63f22011d66c85523ed
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-14 10:22:47 +0200

    Encrypting code added

--- SanityArchive/SanityArchive/MainForm.cs
+++ SanityArchive/SanityArchive/MainForm.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
-namespace SanityArchive
-{
-    public partial class MainForm : Form1
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+namespace SanityArchive {
+    public partial class MainForm : Form1 {
+
+
+        public MainForm() { InitializeComponent(); }
+
+		private void EncryptButton (object sender, EventArgs e) {
+			List<FileInfo> selectedFiles = new List<FileInfo>();
+			selectedFiles.Add(new FileInfo("C:/GDrive/fixip.jpg"));
+	        MessageBox.Show(selectedFiles[0].ToString());
+
+			Stream stream = new FileStream("C:/GDrive/fixip.jpg", FileMode.Create);
+			Rijndael rijAlg = Rijndael.Create();
+
+			rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+			CryptoStream crStream = new CryptoStream(stream, );
+		}
+
+	    public byte[] Key { get; set; }
     }
 }
